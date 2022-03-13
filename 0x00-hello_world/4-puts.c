16bd8c9d0766456bb2d50d029a8006a6822fa950
Precious2912 <preciousovat@gmail.com>
2022-03-13 06:22:49 -0700

    printing output with puts function

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point 
+ *
+ * Return: 0 if exited properly, non-zero otherwise
+ */
+int main(void)
+{
+	puts("\"Programming is like buildimg a multilingual puzzle");
+	return (0);
+}
