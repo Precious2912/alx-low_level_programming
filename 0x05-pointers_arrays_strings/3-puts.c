20ee187524b3805f07dc65e05ff6a26e052fd639
Precious2912 <preciousovat@gmail.com>
2022-03-22 08:45:10 -0700

    print string to stdout

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -10,7 +10,7 @@ void _puts(char *str)
 {
 	int i = 0;
 
-	while(str[i] != '\0')
+	while (str[i] != '\0')
 	{
 		_putchar(str[i]);
 		i++;
@@ -18,4 +18,3 @@ void _puts(char *str)
 
 	_putchar('\n');
 }
-
