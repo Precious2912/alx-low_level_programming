3a844146c62fb88b750ca3cbe9940428d06df7f3
Precious2912 <preciousovat@gmail.com>
2022-03-24 01:38:18 -0700

    concatenate two strings with n bytes from src

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -7,20 +7,22 @@
  * @n: amout of bytes used from src
  * Return: the pointer to dest.
  */
-
 char *_strncat(char *dest, char *src, int n)
 {
 	int count = 0, count2 = 0;
 
-	while (dest[count++])
+	while (*(dest + count) != '\0')
 	{
+		count++;
+	}
+
+	while (count2 < n)
+	{
+		*(dest + count) = *(src + count2);
+		if (*(src + count2) == '\0')
+			break;
+		count++;
 		count2++;
 	}
-
-	for (count = 0; src[count] && count < n; count++)
-	{
-		dest[count2++] = src[count];
-	}
-
 	return (dest);
 }
