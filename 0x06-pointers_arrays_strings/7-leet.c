a64b401b173842c821643fa0a07a78ddd9374d87
Precious2912 <preciousovat@gmail.com>
2022-03-24 07:53:43 -0700

    encodes a string into 1337

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * leet - encodes a string into 1337
+ * @s: string to be encoded
+ * Return: A pointer to encoded string
+ */
+char *leet(char *s)
+{
+	int i = 0, j;
+	char letters[9] = {'O', 'L', '?', 'E', 'A', '?', '?', '?' 'T'};
+
+	while (s[i])
+	{
+		for (j = 0; j <= 7; j++)
+		{
+			if ((s[i] == letters[j]) || (s[i] - 32 == letters[j]))
+				s[i] = j + '0';
+		}
+
+		i++;
+	}
+
+	return (s);
+}
