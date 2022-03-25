b0226442ff3070a1beb67e01cfa5bd1d765952be
Precious2912 <preciousovat@gmail.com>
2022-03-24 19:57:38 -0700

    Capitalize all words of a string

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -7,14 +7,17 @@
  */
 char *cap_string(char *s)
 {
-	int i = 0;
+	int i;
 
+	i = 0;
 	while (s[i])
 	{
 		while (!(s[i] >= 'a' && s[i] <= 'z'))
+		{
 			i++;
+		}
 
-		if (s[i-1] == ' ' ||
+		if (s[i - 1] == ' ' ||
 				s[i - 1] == '\t' ||
 				s[i - 1] == '\n' ||
 				s[i - 1] == ',' ||
@@ -29,6 +32,7 @@ char *cap_string(char *s)
 				s[i - 1] == '}' ||
 				i == 0)
 			s[i] = s[i] - 32;
+
 		i++;
 	}
 
